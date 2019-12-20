d7dfcbf42b5de30b38d95ae8a1025857e45a0df9
Jack_EG <JCooper@executive-grapevine.com>
2019-12-20 14:18:51 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState } from 'react'
+import Navbar from './components/layout/Navbar'
+import Users from './components/users/Users'
+import User from './components/users/User'
+import Search from './components/users/Search'
+import Alert from './components/layout/Alert'
+import About from './components/pages/About'
+import GithubState from './context/github/GithubState'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import './App.css'
 
-function App() {
+const App = () => {
+  const [alert, setAlert] = useState(null)
+
+  const showAlert = (msg, type) => {
+    setAlert({msg, type})
+    setTimeout(() => setAlert(null), 3000)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <GithubState>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+          <Alert alert={alert} />
+          <Switch>
+            <Route exact path={'/'} render={props => (
+              <Fragment>
+                <Search 
+                  setAlert={showAlert} 
+                />
+                <Users />
+              </Fragment>
+            )}/>
+            <Route exact path='/about' component={About} />
+            <Route exact path={`/user/:login`} component={User} />
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    </GithubState>
+  )
 }
 
-export default App;
+export default App
